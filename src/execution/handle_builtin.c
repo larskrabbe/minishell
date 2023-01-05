d0995285a18fd9fc1d3c5cb25f3715e0e1af1d1a
Babajide Ogunlana <bogunlan@3-D-2.42heilbronn.de>
2023-01-05 18:46:13 +0100

    builtin handler added

---
+++ src/execution/handle_builtin.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_builtin.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bogunlan <bogunlan@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/05 18:42:39 by bogunlan          #+#    #+#             */
+/*   Updated: 2023/01/05 18:42:44 by bogunlan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+char	*string_to_lower(char *str)
+{
+	char	*new_str;
+	int		i;
+
+	if (!str)
+		return (NULL);
+	new_str = (char *) malloc(ft_strlen(str) + 1);
+	if (!new_str)
+		return (NULL);
+	i = 0;
+	while (*str)
+	{
+		new_str[i] = ft_tolower(*str);
+		i++;
+		str++;
+	}
+	printf("%s\n", new_str);
+	return (new_str);
+}
+
+int	has_upper_case(char *str)
+{
+	if (!str)
+		return (FALSE);
+	while (*str)
+	{
+		if (*str >= 'A' && *str <= 'Z')
+			return (TRUE);
+		str++;
+	}
+	return (FALSE);
+}
+
+int	find_builtin_match(char *cmd, char *builtin)
+{
+	if (ft_strncmp(cmd, builtin, ft_strlen(cmd)) == 0)
+	{
+		if (ft_strncmp(cmd, builtin, ft_strlen(builtin)) == 0)
+		{
+			return (TRUE);
+		}
+	}
+	return (FALSE);
+}
+
+int	handle_builtin(char *cmd)
+{
+	char	*command;
+
+	command = string_to_lower(cmd);
+	if (!command)
+		return (FALSE);
+	if (find_builtin_match(command, "echo")
+		|| find_builtin_match(command, "cd")
+		|| find_builtin_match(command, "pwd")
+		|| find_builtin_match(command, "env")
+		|| (find_builtin_match(command, "export") && !has_upper_case(cmd))
+		|| (find_builtin_match(command, "unset") && !has_upper_case(cmd))
+		|| (find_builtin_match(command, "exit") && !has_upper_case(cmd)))
+	{
+		free(command);
+		return (TRUE);
+	}
+	free(command);
+	return (FALSE);
+}
